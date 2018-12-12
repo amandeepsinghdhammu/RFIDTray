ff3cd435968b163191eeac32638da47264050176
Amandeep Singh <as.smartdevelopers@gmail.com>
2018-12-12 18:04:16 +0530

    Update nrf.py with only print

--- v3/nrf.py
+++ v3/nrf.py
@@ -1,16 +1,11 @@
 #!/usr/bin/env python
-
+import sys
 import RPi.GPIO as GPIO
 from lib_nrf24 import NRF24
 import time
 import spidev
-from omxplayer.player import OMXPlayer
-from pathlib import Path
 from time import sleep
 import signal
-
-player=''
-playerRunning= False
 
 GPIO.setmode(GPIO.BCM)
 
@@ -31,81 +26,43 @@ radio.enableDynamicPayloads()
 radio.enableAckPayload()
 
 radio.openReadingPipe(1, pipes[1])
-radio.printDetails()
+#radio.printDetails()
 radio.stopListening()
 radio.startListening()
 
-def playVideo(id):
-	global player
-	global playerRunning
-	VIDEO_PATH = Path("/home/pi/NRF24/NRF24l01/videos/" + id + ".mp4")
-	if VIDEO_PATH.is_file():
-		player = OMXPlayer(VIDEO_PATH, args=['--loop'])
-		playerRunning = True
-	else:
-		print("File not found");
-		
-def stopVideo():
-	global playerRunning
-	global player
-	#if 'player' in globals():
-	print('playerRunning')
-	print(playerRunning)
-	print('player')
-	print(player)
-	if playerRunning:
-		playerRunning = False
-		player.quit()
-	
-lastid = "";
 runProgram = True;
 
 try:
     while runProgram:
-        #print(radio.available(0))
         while not radio.available(0):
             time.sleep(1 / 100)
         receivedMessage = []
         radio.read(receivedMessage, radio.getDynamicPayloadSize())
-        #print("Received: {}".format(receivedMessage))
 
-        #print("Translating the receivedMessage into unicode characters")
         string = ""
-
+        data = []
         for n in receivedMessage:
             # Decode into standard unicode set
             if (n >= 32 and n <= 126):
                 string += chr(n)
-        #print("Out received message decodes to: {}".format(string))
-        
+        #print(string)        
         radio.stopListening()
         radio.startListening()
 
-        data = string.split('-')
+        if(string != ""):
+          data = string.split('-')
+
+        if(len(data) > 0):
+            print(string)
+            sys.stdout.flush()
         
-        print(data)
-        # if(string == "stop"):
-        #     stopVideo()
-        #     lastid = ""
-        # else:
-        #     if(string != lastid):
-        #         if(lastid != ""):
-        #             stopVideo()
-        #         lastid = string
-        #         playVideo(string)
-        
-        time.sleep(1)
+        #time.sleep(1)
 
 except KeyboardInterrupt:
     GPIO.cleanup()
 
 # Capture SIGINT for cleanup when the script is aborte
 def end_read(signal,frame):
-    global runProgram
-    global player
-    runProgram = False
-    #print "Ctrl+C captured, ending read."
-    player.quit()
     GPIO.cleanup()
 
 # Hook the SIGINT
